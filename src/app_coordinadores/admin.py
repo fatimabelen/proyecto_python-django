0d8a268de182f051395c0d1f4fb7e8e4f6be8248
Yasmin Burrut <nawsper@gmail.com>
2023-12-01 15:53:24 -0300

    Se registra modelo coordinador

--- src/app_coordinadores/admin.py
+++ src/app_coordinadores/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Coordinador
 
 # Register your models here.
+
+@admin.register(Coordinador)
+class CoordinadorAdmin(admin.ModelAdmin):
+    list_display = ['apellido', 'nombre', 'número_documento', 'fecha_alta', 'activo']
+    search_fields =  ['apellido', 'nombre']
+    list_filter = ['activo']
