2fea340ac8bdec57eec14da408e25460d577c920
Yasmin Burrut <nawsper@gmail.com>
2023-12-09 11:40:42 -0300

    Se generan links para coordinadore

--- src/app_coordinadores/views.py
+++ src/app_coordinadores/views.py
@@ -31,8 +31,9 @@ def actualizar_coordinador(request, id):
 # Crear nuevo coordinador
 class CoordinadorCreateView(generic.CreateView):
     model = Coordinador
-    fields = ['nombre', 'apelliido', 'numero_documento', 'fecha_alta']
-    template_name = 'coordinadores/nuevo_coordinador.html'
+    fields = ['nombre', 'apellido', 'numero_documento']
+    template_name = 'coordinadores/formulario_coordinadores.html'
+    extra_context = {'titulo':'REGISTRAR COORDINADOR', 'mensaje_boton':'REGISTRAR COORDINADOR'}
     success_url = reverse_lazy('coordinadores:listado_coordinadores')
 
 
@@ -41,5 +42,12 @@ def activar_coordinador(request, id):
     coordinador = get_object_or_404(Coordinador, id=id)
     coordinador.activo = True
     coordinador.save()  # Guarda los cambios en la base de datos
-    return redirect('coordinadores:listado:coordinadores')
+    return redirect('coordinadores:listado_coordinadores')
 
+
+# Vista para desactivar el registro de un coordinador
+def desactivar_coordinador(request, id):
+    coordinador = get_object_or_404(Coordinador, id=id)
+    coordinador.activo = False
+    coordinador.save()  # Guarda los cambios en la base de datos
+    return redirect('coordinadores:listado_coordinadores') 
