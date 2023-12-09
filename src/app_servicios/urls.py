a3a8609fcecccc40f51ad82ee7685fae0b7d97a1
Yasmin Burrut <nawsper@gmail.com>
2023-12-09 13:21:35 -0300

    Se crea view para activar servicio

--- src/app_servicios/urls.py
+++ src/app_servicios/urls.py
@@ -5,4 +5,5 @@ app_name = 'app_servicios'
 urlpatterns = [
     path('listado/', views.ServiciosListView.as_view(), name='listado_servicios'),
     path('modificar/<int:id>', views.ServiciosUpdateView.as_view(), name='actualizar_servicio'),
+    path('activar/<int:id>', views.activar_servicio, name="activar_servicio"),
 ]
